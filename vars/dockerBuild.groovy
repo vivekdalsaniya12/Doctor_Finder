7eea07514dd8c92019542243ce2297c0e76018c6
Dalsaniya Vivek <168078146+vivekdalsaniya12@users.noreply.github.com>
2025-05-07 14:46:36 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,4 @@
 def call(Map x){
-  sh 'docker build -t ${x.dockerimage}:${x.imagetag} ${x.path}'
+  // sh 'docker build -t ${x.dockerimage}:${x.imagetag} ${x.path}'
+  sh 'docker build -t vivekdalsaniya/doctor_finder:latest .'
 }
