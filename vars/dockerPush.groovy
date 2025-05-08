a34ee53f3b86c9750e7cebdda7c995a3d072f79d
Dalsaniya Vivek <168078146+vivekdalsaniya12@users.noreply.github.com>
2025-05-08 10:54:03 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,1 +1,8 @@
-
+withCredentials ([usernamePassword(credentialsId:'dockercreds',usernameVariable:'USERNAME',passwordVariable:'PASSWORD')]) 
+{
+  sh '''
+      echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
+      docker push $DOCKER_IMAGE:$IMAGE_TAG
+      docker logout
+  '''
+}
